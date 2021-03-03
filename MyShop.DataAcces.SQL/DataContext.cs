4483be0557397ca17cd41d85de02db09d54c0742
joedrica98 <joaquin.rivadeneira98@gmail.com>
2021-03-02 22:51:41 -0500

    Register Customers

--- MyShop.DataAcces.SQL/DataContext.cs
+++ MyShop.DataAcces.SQL/DataContext.cs
@@ -20,6 +20,7 @@ namespace MyShop.DataAcces.SQL
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
 
     }
