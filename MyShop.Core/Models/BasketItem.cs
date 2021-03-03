e3313878a69e830d470352b4bc171c4e3d2fb93b
joedrica98 <joaquin.rivadeneira98@gmail.com>
2021-03-02 19:03:42 -0500

    Create Basket Service

--- MyShop.Core/Models/BasketItem.cs
+++ MyShop.Core/Models/BasketItem.cs
@@ -8,7 +8,7 @@ namespace MyShop.Core.Models
 {
     public class BasketItem : BaseEntity
     {
-        public string BaskId { get; set; }
+        public string BasketId { get; set; }
         public string ProductId { get; set; }
         public int Quantity { get; set; }
     }
