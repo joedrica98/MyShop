f516c37f759abcab6f00a54da0200369afa293a6
joedrica98 <joaquin.rivadeneira98@gmail.com>
2021-03-02 22:32:25 -0500

    Basket Tests

---
+++ MyShop.WebUI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using MyShop.WebUI;
+using MyShop.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoriesContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+            
+            HomeController controller = new HomeController(productContext, productCategoriesContext);
+
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(1, viewModel.Products.Count());
+        }
+    }
+}
